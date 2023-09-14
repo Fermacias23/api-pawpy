90889989fcbb3b56d07f8c35f1d2919c4b6c7fa0
appsTorreon <developer@appstorreon.com>
2023-09-13 19:31:39 -0600

    sustom-order

--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -1,9 +1,59 @@
-'use strict';
+"use strict";
+const stripe = require("stripe")(
+  // "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"
+  "sk_test_51JBm75EtuCIyjulJux3iHYQWJvCkQYGZcPpRzQrL8oTKQJopSzmZcZzGYiJTecqCRHxkk8T3GWXSgV1MrYdpB7GR0019oK1aBj"
+);
+
+function calcDiscountPrice(price, discount) {
+  if (!discount) return price;
+
+  const discountAmount = (price * discount) / 100;
+  const result = price - discountAmount;
+
+  return result.toFixed(2);
+}
 
 /**
  * order controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::order.order');
+module.exports = createCoreController("api::order.order", ({ strapi }) => ({
+  async paymentOrder(ctx) {
+    const { token, products, userId, addressShipping } = ctx.request.body;
+
+    let totalPayment = 0;
+    products.forEach((product) => {
+      const priceTemp = calcDiscountPrice(product.price, product.discount);
+      totalPayment += Number(priceTemp) * product.quantity;
+    });
+
+    const charge = await stripe.charges.create({
+      amount: Math.round(totalPayment * 100),
+      currency: "mxn",
+      source: token,
+      description: `User ID: ${userId}`,
+    });
+
+    const data = {
+      products,
+      user: userId,
+      totalPayment,
+      idPayment: charge.id,
+      addressShipping,
+    };
+
+    const model = strapi.contentTypes["api::order.order"];
+    const validData = await strapi.entityValidator.validateEntityCreation(
+      model,
+      data
+    );
+
+    const entry = await strapi.db
+      .query("api::order.order")
+      .create({ data: validData });
+
+    return entry;
+  },
+}));
