d7dabf42117b2c92b045c159433a556400c98b59
appsTorreon <86739492+Fermacias23@users.noreply.github.com>
2023-10-20 11:24:23 -0600

    delete-account

--- src/api/delete-account/controllers/delete.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  async delete(ctx) {
-    const { user } = ctx.state;
-
-    await strapi.query("user", "users-permissions").delete({ id: user.id });
-
-    return ctx.send({ message: "Account deleted successfully" });
-  },
-};
